fb21e6257d19f0a34c526a4897d0ef563a8103e0
Gilles Gautier <gillesgweb@gmail.com>
2022-03-09 14:39:05 +0100

    foreach loop corrected in Models/Item/Index

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly IEnumerable<Item> _items;
+
+        public ItemController(IEnumerable<Item> items)
+        {
+            _items = items;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_items.ToList());
+        }
+    }
+}
