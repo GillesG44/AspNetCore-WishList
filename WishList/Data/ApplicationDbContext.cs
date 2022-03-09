a1a367eaffe6727ef662f0b8d20a2534cc23c334
Gilles Gautier <gillesgweb@gmail.com>
2022-03-09 11:10:16 +0100

    ApplicationDbContext and Items Added

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
